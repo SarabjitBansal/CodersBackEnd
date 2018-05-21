8bb36ef33940510d00d97c80a9c160e21357f863
sonia08 <soniabansal@Sonias-MacBook-Pro.local>
2018-05-21 14:23:59 +1000

    hnages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.gem 'redis'
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.url = 'https://codersappserver.herokuapp.com/cable'
+  config.actioncable-cable.allowed_request_origins=['https://codersapp.herokuapp.com','http://localhost:3333']
 end
